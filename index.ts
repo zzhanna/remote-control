7130d2269f3a8c9b42154bd3841555f3d933df7c
Zhanna Bohan <foto1drug@gmail.com>
2023-01-21 21:59:19 +0300

    refactor: add typescript, nodemon, ws, nutjs.dev, jimp, cross-env, eslint, prettier, rename files

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from "./src/http_server/index";
 import { mouse } from "@nut-tree/nut-js";
+import "dotenv/config";
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = process.env.PORT || 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
